9242dafeace4babdb1a52718412ff70d5ba24ff7
Fernando Gallo <fernandgallo@gmail.com>
2023-08-08 16:25:36 -0300

    Fix issue where access to device's gallery was not being requested

--- CameraTok/CameraTok/Video Gallery PhotoKit/LocalVideoGalleryLoader.swift
+++ CameraTok/CameraTok/Video Gallery PhotoKit/LocalVideoGalleryLoader.swift
@@ -7,20 +7,27 @@ import Foundation
 public final class LocalVideoGalleryLoader: VideoGalleryLoader {
     private let videoLibrary: VideoLibrary
     
-    init(videoLibrary: VideoLibrary) {
+    public init(videoLibrary: VideoLibrary) {
         self.videoLibrary = videoLibrary
     }
     
     public func load(with startDate: Date, completion: @escaping (VideoGalleryLoader.Result) -> Void) {
-        guard videoLibrary.isPhotoAccessAuthorized() else {
-            let error = NSError(domain: "Access to device gallery is not allowed.", code: 0)
-            completion(.failure(error))
-            return
+        if !videoLibrary.isPhotoAccessAuthorized() {
+            videoLibrary.requestAuthorization { authorized in
+                if authorized {
+                    completion(.success(self.fetchAssetsAndMapToModel(startDate)))
+                } else {
+                    completion(.failure(NSError(domain: "Please, you need to allow access to your gallery.", code: 0)))
+                }
+            }
+        } else {
+            completion(.success(fetchAssetsAndMapToModel(startDate)))
         }
-        
+    }
+    
+    private func fetchAssetsAndMapToModel(_ startDate: Date) -> [VideoItem] {
         let assets = videoLibrary.fetchVideoAssets(startDate: startDate)
         let videoItems = assets.map { VideoItem(id: $0.localIdentifier, duration: $0.duration, liked: false) }
-        
-        completion(.success(videoItems))
+        return videoItems
     }
 }
