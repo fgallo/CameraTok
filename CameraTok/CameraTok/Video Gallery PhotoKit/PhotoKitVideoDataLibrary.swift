a82536576e15a152bc5ba041985ccfdcdffb5591
Fernando Gallo <fernandgallo@gmail.com>
2023-08-08 16:11:07 -0300

    Change `PhotoKitVideoDataLibrary` properties and functions to public, to be used outside the module

--- CameraTok/CameraTok/Video Gallery PhotoKit/PhotoKitVideoDataLibrary.swift
+++ CameraTok/CameraTok/Video Gallery PhotoKit/PhotoKitVideoDataLibrary.swift
@@ -6,18 +6,20 @@ import Foundation
 import Photos
 import PhotosUI
 
-final class PhotoKitVideoDataLibrary: VideoDataLibrary {
-    func fetchVideoAsset(id: String) -> PHAsset {
+public final class PhotoKitVideoDataLibrary: VideoDataLibrary {
+    public init() {}
+    
+    public func fetchVideoAsset(id: String) -> PHAsset {
         let result = PHAsset.fetchAssets(withLocalIdentifiers: [id], options: nil)
         let asset = result.object(at: 0)
         
         return asset
     }
     
-    func getThumbnailDataFromAsset(_ asset: PHAsset, size: CGSize, completion: @escaping (Data?) -> Void) {
+    public func getThumbnailDataFromAsset(_ asset: PHAsset, size: CGSize, completion: @escaping (Data?) -> Void) {
         let imageManager = PHCachingImageManager()
         imageManager.requestImage(for: asset, targetSize: size, contentMode: .aspectFill, options: nil) { image, info in
-            completion(image?.tiffRepresentation)
+            completion(image?.pngData())
         }
     }
 }
