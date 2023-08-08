dff5cc27ff14fab67a221dba19f11970fff8b93b
Fernando Gallo <fernandgallo@gmail.com>
2023-08-07 23:48:39 -0300

    Add CameraTokApp module

---
+++ CameraTokApp/CameraTokApp/CameraTokAppApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Fernando Gallo on 07/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraTokAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
