4396e49537017d391981e28a65b8b24b73d563bf
Fernando Gallo <fernandgallo@gmail.com>
2023-08-09 16:38:26 -0300

    Create `VideoPlayerView` to use on Feed

---
+++ CameraTokApp/CameraTokApp/Feed UI/Views/VideoPlayer/VideoPlayerView.swift
@@ -0,0 +1,165 @@
+//
+//  Created by Fernando Gallo on 09/08/23.
+//
+
+import SwiftUI
+import AVFoundation
+
+class VideoPlayerUIView: UIView {
+    private let player: AVQueuePlayer
+    private let playerLayer = AVPlayerLayer()
+    private var playerLooper: AVPlayerLooper?
+    private let videoPos: Binding<Double>
+    private let videoDuration: Binding<Double>
+    private let seeking: Binding<Bool>
+    private var durationObservation: NSKeyValueObservation?
+    private var timeObservation: Any?
+  
+    init(player: AVQueuePlayer, videoPos: Binding<Double>, videoDuration: Binding<Double>, seeking: Binding<Bool>) {
+        self.player = player
+        self.videoDuration = videoDuration
+        self.videoPos = videoPos
+        self.seeking = seeking
+        
+        super.init(frame: .zero)
+    
+        backgroundColor = .black
+        playerLayer.player = player
+        playerLayer.videoGravity = .resizeAspectFill
+        layer.addSublayer(playerLayer)
+        
+        if let videoURL = ((player.currentItem?.asset) as? AVURLAsset)?.url {
+            let videoItem = AVPlayerItem(url: videoURL)
+            playerLooper = AVPlayerLooper(player: player, templateItem: videoItem)
+        }
+        
+        // Observe the duration of the player's item so we can display it
+        // and use it for updating the seek bar's position
+        durationObservation = player.currentItem?.observe(\.duration, changeHandler: { [weak self] item, change in
+            guard let self = self else { return }
+            self.videoDuration.wrappedValue = item.duration.seconds
+        })
+        
+        // Observe the player's time periodically so we can update the seek bar's
+        // position as we progress through playback
+        timeObservation = player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 0.5, preferredTimescale: 600), queue: nil) { [weak self] time in
+            guard let self = self else { return }
+            // If we're not seeking currently (don't want to override the slider
+            // position if the user is interacting)
+            guard !self.seeking.wrappedValue else {
+                return
+            }
+        
+            // update videoPos with the new video time (as a percentage)
+            self.videoPos.wrappedValue = time.seconds / self.videoDuration.wrappedValue
+        }
+    }
+  
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+  
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    
+        playerLayer.frame = bounds
+    }
+    
+    func cleanUp() {
+        // Remove observers we setup in init
+        durationObservation?.invalidate()
+        durationObservation = nil
+        
+        if let observation = timeObservation {
+            player.removeTimeObserver(observation)
+            timeObservation = nil
+        }
+    }
+}
+
+// This is the SwiftUI view that contains the controls for the player
+struct VideoPlayerControlsView : View {
+    @Binding private(set) var videoPos: Double
+    @Binding private(set) var videoDuration: Double
+    @Binding private(set) var seeking: Bool
+    
+    let player: AVPlayer
+    
+    @State private var playerPaused = true
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("\(TimeFormatter.formatSecondsToHMS(videoPos * videoDuration))")
+                Text(" / ")
+                Text("\(TimeFormatter.formatSecondsToHMS(videoDuration))")
+            }
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            
+            Slider(value: $videoPos, in: 0...1, onEditingChanged: sliderEditingChanged)
+                .tint(.white)
+        }
+        .padding()
+    }
+    
+    private func togglePlayPause() {
+        pausePlayer(!playerPaused)
+    }
+    
+    private func pausePlayer(_ pause: Bool) {
+        playerPaused = pause
+        if playerPaused {
+            player.pause()
+        }
+        else {
+            player.play()
+        }
+    }
+    
+    private func sliderEditingChanged(editingStarted: Bool) {
+        if editingStarted {
+            // Set a flag stating that we're seeking so the slider doesn't
+            // get updated by the periodic time observer on the player
+            seeking = true
+            pausePlayer(true)
+        }
+        
+        // Do the seek if we're finished
+        if !editingStarted {
+            let targetTime = CMTime(seconds: videoPos * videoDuration,
+                                    preferredTimescale: 600)
+            player.seek(to: targetTime) { _ in
+                // Now the seek is finished, resume normal operation
+                self.seeking = false
+                self.pausePlayer(false)
+            }
+        }
+    }
+}
+
+struct VideoPlayerView: UIViewRepresentable {
+    @Binding private(set) var videoPos: Double
+    @Binding private(set) var videoDuration: Double
+    @Binding private(set) var seeking: Bool
+    
+    let player: AVQueuePlayer
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<VideoPlayerView>) {}
+    
+    func makeUIView(context: UIViewRepresentableContext<VideoPlayerView>) -> UIView {
+        let uiView = VideoPlayerUIView(player: player,
+                                       videoPos: $videoPos,
+                                       videoDuration: $videoDuration,
+                                       seeking: $seeking)
+        return uiView
+    }
+    
+    static func dismantleUIView(_ uiView: UIView, coordinator: ()) {
+        guard let playerUIView = uiView as? VideoPlayerUIView else {
+            return
+        }
+        
+        playerUIView.cleanUp()
+    }
+}
